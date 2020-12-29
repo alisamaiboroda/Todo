57e1916ec2ab8f475c468e4f262af41710689b6a
Alisa <alisa.maiboroda88@gmail.com>
2020-12-29 15:54:54 +0200

    create todos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,23 @@
 <template>
   <ul>
-    <TodoItem />
+    <TodoItem v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo" />
   </ul>
 </template>
 
 <script>
 import TodoItem from "./TodoItem";
 export default {
+  props: ["todos"],
   components: {
     TodoItem,
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
